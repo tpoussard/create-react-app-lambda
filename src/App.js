e167ac6e67db1fb2982262c3673cdb56caf5b385
timothee.poussard <timothee.poussard@yahoo.com>
2020-01-13 22:28:07 +0100

    api down switch to quick patch for deploy netlify bis

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.png';
+import logo from './logo.svg';
 import './App.css';
 import Category from './components/Category_component';
 
